61cda185c8917a413f213d75415c2a42c9bb437a
David Tacoronte <d.tacoronte89@gmail.com>
2024-09-04 18:26:19 -0400

    hangman done and functional!

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -7,7 +7,7 @@ def get_valid_word(words):
     while '-' in word or ' ' in word:
         word = random.choice(words)
     
-    return word
+    return word.upper()
 
 def hangman():
     word = get_valid_word(words)
@@ -15,22 +15,33 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     
-    while len(word_letters) > 0:
-        print('You have used these letters:', ' '.join(used_letters))
+    lives = 6
+    
+    while len(word_letters) > 0 and lives > 0:
+        print('\nYou have', lives,'lives left, and you have used these letters:', ' '.join(used_letters))
         
         word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('Current word: ', ' '.join(word_list))
+        print('Current word: ', ' '.join(word_list),'\n')
         
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                
+            else:
+                lives = lives - 1
+                print('Letter is not in the word.')
         
         elif user_letter in used_letters:
             print('You have already used that character. Please try again.')
 
         else:
             print('Invalid character. Please try again.')
+            
+    if lives == 0:
+        print('Oh no, you died. The word was ', word, '.')
+    else:
+        print('You guessed the word: ', word, '! Good job.')
 
 hangman()
