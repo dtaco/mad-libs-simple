30155db92330907ddc43a9fcf812df5bd91c90ff
David Tacoronte <d.tacoronte89@gmail.com>
2024-09-04 18:06:57 -0400

    hangman working, but has error in connecting guess and answer

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,15 +15,22 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-    
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
+    while len(word_letters) > 0:
+        print('You have used these letters:', ' '.join(used_letters))
+        
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+        
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
 
-    else:
-        print('Invalid character. Please try again.')
-user_input = input('Type something: ')
+        else:
+            print('Invalid character. Please try again.')
+
+hangman()
